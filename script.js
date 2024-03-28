16e26478dc2bd81ae15c17d162d2c266fb6ada57
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-28 18:12:07 -0300

    Perguntando nome e mostrando mensagem

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+    Encontre a solução ao problema:
+
+    Pergunte o nome do usuário e escreva a mensagem:
+    "Olá, [nome do usuário]"
+
+    Como resolver problemas na linguagem de programação?
+    Como melhorar minha lógica de programação?
+
+    1. Identifique o que já sabe e faça!
+    2. Quebreo grande problema em problemas menores!
+ */
+let nome = prompt("Qual o seu nome?");
+alert("Olá, " + nome);
