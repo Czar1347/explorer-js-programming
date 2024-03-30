62a8dccd0158b3e58c94bff86161923ca687a3e2
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-03-30 18:56:07 -0300

    Somar dois números

---
+++ 02/script.js
@@ -0,0 +1,10 @@
+/*
+    Solicite 2 números, faça a soma deles e apresente o resultado final ao usuário
+*/
+alert("Iremos somar os 2 números");
+let numberOne = prompt("Digite o primeiro número: ");
+let numberTwo = prompt("Digite o segundo número: ");
+let resultado = Number(numberOne) + Number(numberTwo)
+// alert((Number(numberOne) + Number(numberTwo)) / 2)
+// alert("Resultado final: " + (Number(numberOne) + Number(numberTwo)))
+alert("Resultado final: " + resultado)
